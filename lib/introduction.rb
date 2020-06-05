c33999968132c50e05c1d762964e17faf5a74e65
Sawandi Wilson <sawandiwilson@gmail.com>
2020-06-05 21:28:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 def introduction_with_language ("Dan", "Ember.js")
-  puts
+  puts"Hi, my name is Dan and I am learning to program in Ember.js.\n"
 end
   
